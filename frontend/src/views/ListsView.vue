1a5bc2341784cd6f817a5b212eab7deccdbc15eb
BenjaminKing1337 <benjaminking1337@live.com>
2022-05-16 14:50:12 +0200

    we did it boise, we figured it out
    it's all coming together

--- frontend/src/views/ListsView.vue
+++ frontend/src/views/ListsView.vue
@@ -5,8 +5,13 @@
       <div class="new">
         <h4>Create New List</h4>
         <input type="text" placeholder="Title" v-model="lState.NewTitle" />
-        <br /> Complete
-        <input type="checkbox" placeholder="Complete" v-model="lState.NewIs_Complete" />
+        <br />
+        Complete
+        <input
+          type="checkbox"
+          placeholder="Complete"
+          v-model="lState.NewIs_Complete"
+        />
         <br />
         <button @click="NewList()">Create New List</button>
       </div>
@@ -20,7 +25,6 @@
       </div>
     </div>
 
-
     <div v-for="List in lState.Lists" :key="List._id" class="listitem">
       <q-card flat bordered class="my-card bg-grey-1">
         <q-card-section>
@@ -31,13 +35,16 @@
                   {{ List.title }}
                 </h4>
               </div>
-              <div class="text-subtitle2">
-                <p>
-                  {{ List.tasks }}
-                </p>
-                <p>
-                  Completed: {{ List.is_Complete }}
-                </p>
+              <div
+                v-for="Task in tState.Tasks"
+                :key="Task._id"
+                class="text-subtitle2 border"
+              >
+                <p>Name:{{ Task.task }}</p>
+                <p>Decription: {{ Task.description }}</p>
+                <p>Deadline: {{ Task.deadline }}</p>
+                <p>Status: {{ Task.status }}</p>
+                <p>Optional?: {{ Task.is_optional }}</p>
               </div>
             </div>
 
@@ -69,63 +76,28 @@
         </q-card-section>
 
         <q-card-section>
-          {{ lorem }}
+          <p>Completed: {{ List.is_Complete }}</p>
         </q-card-section>
 
         <q-separator />
       </q-card>
     </div>
-
-    <!--  <q-card flat bordered class="my-card bg-grey-1">
-      <q-card-section>
-        <div class="row items-center no-wrap">
-          <div class="col">
-            <div class="text-h6">Deploy</div>
-            <div class="text-subtitle2">by Benjamin</div>
-          </div>
-
-          <div class="col-auto">
-            <q-btn color="grey-7" round flat icon="more_vert">
-              <q-menu cover auto-close>
-                <q-list>
-                  <q-item clickable>
-                    <q-item-section>
-                      <button @click="EditList(List.value._id)">
-                        Edit List
-                      </button>
-                    </q-item-section>
-                  </q-item>
-                  <q-item clickable>
-                    <q-item-section>
-                      <button @click="DeleteList(List._id)">Delete List</button>
-                    </q-item-section>
-                  </q-item>
-                </q-list>
-              </q-menu>
-            </q-btn>
-          </div>
-        </div>
-      </q-card-section>
-
-      <q-card-section>
-        {{ lorem }}
-      </q-card-section>
-
-      <q-separator />
-    </q-card> -->
   </q-page>
 </template>
 
 <script>
 import Listcrud from "../modules/listcrud";
+import Taskcrud from "../modules/taskcrud";
 import { onMounted } from "vue";
 
 export default {
   setup() {
     const { lState, GetAllLists, NewList, DeleteList, EditList } = Listcrud();
+    const { tState, GetAllTasks, NewTask, DeleteTask, EditTask } = Taskcrud();
 
     onMounted(() => {
       GetAllLists();
+      GetAllTasks();
     });
 
     // GetAll();
@@ -135,11 +107,21 @@ export default {
       NewList,
       DeleteList,
       EditList,
+      tState,
+      GetAllTasks,
+      NewTask,
+      DeleteTask,
+      EditTask,
     };
   },
 };
 </script>
 
 
-<style lang="sass" scoped>
+<style lang="scss" scoped>
+.new {
+  margin: 1%;
+  padding: 1%;
+  border: 1px solid map-get($cs, 1);
+}
 </style>
