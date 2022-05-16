51c036b247c146a7f0c4cf6b8a95f8d3decb515a
BenjaminKing1337 <benjaminking1337@live.com>
2022-05-16 10:07:51 +0200

    heroku test

---
+++ frontend/src/modules1/taskcrud.js
@@ -0,0 +1,111 @@
+import { ref, computed } from "vue";
+import { useRoute, useRouter } from "vue-router";
+
+const GetTasks = () => {
+  const Route = useRoute();
+  const Router = useRouter();
+
+  const TaskId = computed(() => Route.params.id);
+  console.log("taskId: ", TaskId);
+
+  const tState = ref({
+    NewAuthor: "",
+    NewDescription: "",
+    NewStatus: "",
+    NewIs_Optional: "",
+    NewDeadline: "",
+    NewTaskItem: "",
+    Tasks: {},
+  });
+
+  const GetAllTasks = async () => {
+    try {
+      await fetch("https://sableplan.herokuapp.com/tasks")
+        .then((Res) => Res.json())
+        .then((Data) => {
+          tState.value.Tasks = Data;
+        });
+    } catch (Error) {
+      console.log(Error);
+    }
+  };
+
+  const NewTask = () => {
+    const RequestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        // "auth-token": state.token
+      },
+      body: JSON.stringify({
+        author: tState.value.NewAuthor,
+        description: tState.value.NewDescription,
+        status: tState.value.NewStatus,
+        is_optional: tState.value.NewIs_Optional,
+        deadline: tState.value.NewDeadline,
+        task: tState.value.NewTaskItem,
+      }),
+    };
+    fetch("https://sableplan.herokuapp.com/tasks/new", RequestOptions).then(() => {
+      GetAllTasks(); // Updates page
+    });
+  };
+
+  const DeleteTask = (_id) => {
+    fetch("https://sableplan.herokuapp.com/tasks/delete/" + _id, {
+      method: "DELETE",
+    }).then(() => {
+      GetAllTasks(); // Updates page
+    });
+  };
+
+  const EditTask = () => {
+    const RequestOptions = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        // "auth-token": state.token
+      },
+      body: JSON.stringify({
+        author: tState.value.NewAuthor,
+        description: tState.value.NewDescription,
+        status: tState.value.NewStatus,
+        is_optional: tState.value.NewIs_Optional,
+        deadline: tState.value.NewDeadline,
+        task: tState.value.NewTaskItem,
+      }),
+    };
+    fetch("https://sableplan.herokuapp.com/tasks/update/" + TaskId.value, RequestOptions)
+      .then((Res) => Res.body)
+      .then((Res) => {
+        console.log(Res);
+      });
+    Router.push("/tasks");
+  };
+
+  const Task = ref({});
+  const GetSpecificTask = async () => {
+    try {
+      fetch("https://sableplan.herokuapp.com/tasks/")
+        .then((Res) => Res.json())
+        .then((Data) => {
+          Task.value = Data.filter((T) => T._id === TaskId.value);
+        });
+    } catch (Error) {
+      console.log(Error);
+    }
+  };
+
+  return {
+    Task,
+    TaskId,
+    GetSpecificTask,
+    tState,
+    GetAllTasks,
+    NewTask,
+    DeleteTask,
+    EditTask,
+  };
+};
+
+export default GetTasks;
---
+++ frontend/src/modules1/usercrud.js
@@ -0,0 +1,99 @@
+import { ref, computed } from "vue";
+import { useRoute, useRouter } from "vue-router";
+
+const GetUsers = () => {
+  const Route = useRoute();
+  const Router = useRouter();
+
+  const UserId = computed(() => Route.params.id);
+  // console.log("userId: ", UserId);
+
+  const uState = ref({
+    newName: "",
+    newPass: "",
+    Users: {},
+  });
+  const GetAllUsers = async () => {
+    try {
+      await fetch("https://sableplan.herokuapp.com/users")
+        .then((Res) => Res.json())
+        .then((Data) => {
+          uState.value.Users = Data;
+         
+        });
+    } catch (Error) {
+      console.log(Error);
+    }
+  };
+
+  // const NewUser = () => {
+  //   const RequestOptions = {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //       // "auth-token": state.token
+  //     },
+  //     body: JSON.stringify({
+  //       author: uState.value.NewAuthor,
+  //       Task: uState.value.NewTaskItem,
+  //     }),
+  //   };
+  //   fetch("https://sableplan.herokuapp.com/tasks/new", RequestOptions).then(() => {
+  //     GetAllTasks(); // Updates page
+  //   });
+  // };
+
+  const DeleteUser = (_id) => {
+    fetch("https://sableplan.herokuapp.com/users/delete/" + _id, {
+      method: "DELETE",
+    }).then(() => {
+      GetAllUsers(); // Updates page
+    });
+  };
+
+  const EditUser = () => {
+    const RequestOptions = {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        // "auth-token": state.token
+      },
+      body: JSON.stringify({
+        name: uState.value.newName,
+        pass: uState.value.newPass,
+      }),
+    };
+    fetch("https://sableplan.herokuapp.com/users/update/" + UserId.value, RequestOptions)
+      .then((Res) => Res.body)
+      .then((Res) => {
+        console.log(Res);
+      });
+    Router.push("/users");
+  };
+
+  const User = ref({});
+  const GetSpecificUser = async () => {
+    try {
+      fetch("https://sableplan.herokuapp.com/users/")
+        .then((Res) => Res.json())
+        .then((Data) => {
+          User.value = Data.filter((U) => U._id === UserId.value);
+        });
+    } catch (Error) {
+      console.log(Error);
+    }
+  };
+
+  return {
+    User,
+    UserId,
+    GetSpecificUser,
+    uState,
+    GetAllUsers,
+    // NewUser,
+    DeleteUser,
+    EditUser,
+  };
+};
+
+export default GetUsers;
