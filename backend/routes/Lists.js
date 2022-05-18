b2d8e6c75719a95d8e9e19ce25446a62db439442
BenjaminKing1337 <benjaminking1337@live.com>
2022-05-18 18:56:58 +0200

    catch fish

--- backend/routes/Lists.js
+++ backend/routes/Lists.js
@@ -10,33 +10,48 @@ Router.get("/", async (Req, Res) => {
 
 // Create new List route
 Router.post("/new", async (Req, Res) => {
-  const NewList = new List(
-    Req.body // What the Vue App is sending
-  );
-  const SavedList = await NewList.save(); // mongo save method
-  Res.json(SavedList); // respond with json to our post endpoint
+  try {
+    const NewList = new List(
+      Req.body // What the Vue App is sending
+    );
+    const SavedList = await NewList.save(); // mongo save method
+    Res.json(SavedList); // respond with json to our post endpoint
+  } catch (fish) {
+    Res.status(400).json({ fish });
+  }
 });
 
 // Get List by id route
 Router.get("/get/:id", async (Req, Res) => {
-  const IdList = await List.findById({ _id: Req.params.id });
-  Res.json(IdList);
+  try {
+    const IdList = await List.findById({ _id: Req.params.id });
+    Res.json(IdList);
+  } catch (fish) {
+    Res.status(400).json({ fish });
+  }
 });
 
 // Delete List by id route
 Router.delete("/delete/:id", async (Req, Res) => {
-  const DelList = await List.findByIdAndDelete({ _id: Req.params.id });
-  Res.json(DelList);
+  try {
+    const DelList = await List.findByIdAndDelete({ _id: Req.params.id });
+    Res.json(DelList);
+  } catch (fish) {
+    Res.status(400).json({ fish });
+  }
 });
 
 // Update List by id route
 Router.put("/update/:id", async (Req, Res) => {
-  const UpdList = await List.updateOne(
-    { _id: Req.params.id },
-    { $set: Req.body },
-    
-  );
-  Res.json(UpdList);
+  try {
+    const UpdList = await List.updateOne(
+      { _id: Req.params.id },
+      { $set: Req.body }
+    );
+    Res.json(UpdList);
+  } catch (fish) {
+    Res.status(400).json({ fish });
+  }
 });
 
 module.exports = Router;
--- backend/routes/Tasks.js
+++ backend/routes/Tasks.js
@@ -4,31 +4,42 @@ const Task = require("../models/Tasks");
 
 // Get all Task routes
 Router.get("/", async (Req, Res) => {
-  const Tasks = await Task.find();
-  Res.json(Tasks);
+  try {
+    const Tasks = await Task.find();
+    Res.json(Tasks);
+  } catch (fish) {
+    Res.status(400).json({ fish });
+  }
 });
 
 // Create new Task route
 Router.post("/new", async (Req, Res) => {
-  const NewTask = new Task(
-    Req.body // What the Vue App is sending
-  );
-  const SavedTask = await NewTask.save(); // mongo save method
-  Res.json(SavedTask); // respond with json to our post endpoint
+  try {
+    const NewTask = new Task(
+      Req.body // What the Vue App is sending
+    );
+    const SavedTask = await NewTask.save(); // mongo save method
+    Res.json(SavedTask); // respond with json to our post endpoint
+  } catch (fish) {
+    Res.status(400).json({ fish });
+  }
 });
 
 // Get Task by id route
 Router.get("/get/:id", async (Req, Res) => {
-  const IdTask = await Task.findById({ _id: Req.params.id });
-  Res.json(IdTask);
+  try {
+    const IdTask = await Task.findById({ _id: Req.params.id });
+    Res.json(IdTask);
+  } catch (fish) {
+    Res.status(400).json({ fish });
+  }
 });
 // Get Task by List id route
 Router.get("/get/byList/:id", async (Req, Res) => {
   try {
     const IdList = await Task.find({ ListId: Req.params.id });
     Res.json(IdList);
-  }
-  catch (fish) {
+  } catch (fish) {
     Res.status(400).json({ fish });
   }
 });
@@ -37,9 +48,8 @@ Router.get("/get/byList/:id", async (Req, Res) => {
 Router.delete("/delete/:id", async (Req, Res) => {
   try {
     const DelTask = await Task.findByIdAndDelete({ _id: Req.params.id });
-  Res.json(DelTask);
-  }
-  catch (fish) {
+    Res.json(DelTask);
+  } catch (fish) {
     Res.status(400).json({ fish });
   }
 });
@@ -48,7 +58,7 @@ Router.delete("/delete/:id", async (Req, Res) => {
 Router.put("/update/:id", async (Req, Res) => {
   const UpdTask = await Task.updateOne(
     { _id: Req.params.id },
-    { $set: Req.body },
+    { $set: Req.body }
     // {
     //   author: "Bart",
     //   task: "Go Skating",
